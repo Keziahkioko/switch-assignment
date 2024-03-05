cdbcbf0894fd3ff42dfb97b07470fd0f67829f22
Keziahkioko <161559738+Keziahkioko@users.noreply.github.com>
2024-03-05 22:59:52 +0300

    Add files via upload

---
+++ odd numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int number = 53;
+
+    printf("Odd numbers from 53 to 21 (inclusive):\n");
+    while (number >= 21) {
+
+            printf("%d\n", number);
+        number-=53;
+    }
+    return 0;
+}
